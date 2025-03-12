2b505ec574c2bfe0602882ec9bc6466d093fc31f
limcheekin <limcheekin@vobject.com>
2025-03-12 10:30:41 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fastrtc-issue-138
-Repo created specifically for FastRTC Issue #138
+Repo created based on https://huggingface.co/spaces/fastrtc/talk-to-claude specifically for FastRTC Issue #138:
+- https://github.com/freddyaboulton/fastrtc/issues/138
---
+++ app.py
@@ -0,0 +1,104 @@
+import json
+import os
+from pathlib import Path
+
+import gradio as gr
+import numpy as np
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastrtc import (
+    AdditionalOutputs,
+    ReplyOnPause,
+    Stream,
+    get_twilio_turn_credentials,
+)
+from gradio.utils import get_space
+from pydantic import BaseModel
+
+load_dotenv()
+
+curr_dir = Path(__file__).parent
+
+
+
+def response(
+    audio: tuple[int, np.ndarray],
+    chatbot: list[dict] | None = None,
+):
+        # 1. The code below for PCM audio format will work
+        # with open('speech.pcm', 'rb') as file:
+        #     audio_bytes = file.read()
+        # audio_array = np.frombuffer(audio_bytes, dtype=np.int16).reshape(1, -1)
+        # yield (24000, audio_array)
+
+        # 2. The same code doesn't work for OPUS audio format
+        with open('speech.opus', 'rb') as file:
+            audio_bytes = file.read()
+        audio_array = np.frombuffer(audio_bytes, dtype=np.int16).reshape(1, -1)
+        yield (24000, audio_array)     
+ 
+
+chatbot = gr.Chatbot(type="messages")
+stream = Stream(
+    modality="audio",
+    mode="send-receive",
+    handler=ReplyOnPause(response),
+    additional_outputs_handler=lambda a, b: b,
+    additional_inputs=[chatbot],
+    additional_outputs=[chatbot],
+    rtc_configuration=get_twilio_turn_credentials() if get_space() else None,
+    concurrency_limit=5 if get_space() else None,
+    time_limit=90 if get_space() else None,
+)
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class InputData(BaseModel):
+    webrtc_id: str
+    chatbot: list[Message]
+
+
+app = FastAPI()
+stream.mount(app)
+
+
+@app.get("/")
+async def _():
+    rtc_config = get_twilio_turn_credentials() if get_space() else None
+    html_content = (curr_dir / "index.html").read_text()
+    html_content = html_content.replace("__RTC_CONFIGURATION__", json.dumps(rtc_config))
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.post("/input_hook")
+async def _(body: InputData):
+    stream.set_input(body.webrtc_id, body.model_dump()["chatbot"])
+    return {"status": "ok"}
+
+
+@app.get("/outputs")
+def _(webrtc_id: str):
+    async def output_stream():
+        async for output in stream.output_stream(webrtc_id):
+            chatbot = output.args[0]
+            yield f"event: output\ndata: {json.dumps(chatbot[-1])}\n\n"
+
+    return StreamingResponse(output_stream(), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+    import os
+
+    if (mode := os.getenv("MODE")) == "UI":
+        stream.ui.launch(server_port=7860, server_name="0.0.0.0")
+    elif mode == "PHONE":
+        stream.fastphone(host="0.0.0.0", port=7860)
+    else:
+        import uvicorn
+
+        uvicorn.run(app, host="0.0.0.0", port=7860)
